28779cf36d45776ca5d6c9f8a3df3081f0dd20e5
Alexsandra <alexroze12@gmail.com>
2021-10-26 12:59:30 +0300

    practical teacher class

--- lab2/header.h
+++ lab2/header.h
@@ -19,3 +19,15 @@ public:
 	string getSchoolSub() { return schoolSub; }
 	char getSex() { return sex; }
 };
+class practical_teacher : private teacher
+{
+protected:
+	bool grades_for_free_credit;
+	bool attitude_to_attendance;
+
+public:
+	void setGrades_for_free_credit(bool input) { grades_for_free_credit = input; }
+	void setAttitude_to_attendance(bool input) { attitude_to_attendance = input; }
+	bool getGrades_for_free_credit() { return grades_for_free_credit; }
+	bool getAttitude_to_attendance() { return attitude_to_attendance; }
+};
